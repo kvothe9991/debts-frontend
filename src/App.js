2cde5ca86022e1b317114dfa7ad1cbd4dae359f2
kvothe9991 <kvothe9991@gmail.com>
2022-09-26 22:55:19 +0200

    Initial functioning project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { getDebts } from './services';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const debts = getDebts()
+    const [form_opened, set_form_opened] = useState(false);
+
+    // Return view to render.
+    return (
+        <div className='container'>
+            <div className="wrap">
+                <div className="title">Debts</div>
+                <div className="wrap">
+                    {debts.map(e => <Item debt={e}></Item>)}
+                </div>
+            </div>
+            <div className="bottom">
+                <span>TOTAL</span>
+                <span>${debts.reduce((acc, debt) => debt.amount + acc, 0)}</span>
+            </div>
+            <button onClick={() => { set_form_opened(true) }}
+                className="new-debt-button">
+                <div className="add-icon" />
+                Add
+            </button>
+            {form_opened ? <NewDebtForm /> : null}
+        </div>
+    )
+}
+
+function Item({ debt }) {
+    let element_classname = 'element' + (debt.completed ? ' completed' : '');
+    return (
+        <div className={element_classname}>
+            <div className='left'>
+                <div className="ball"></div>
+                <div>
+                    <div>{debt.title}</div>
+                    <div className='details'>{debt.from}</div>
+                    <div className='details'>created: {debt.created}</div>
+                </div>
+            </div>
+            <span>${debt.amount}</span>
+        </div>
+    )
+}
+
+function NewDebtForm() {
+    const [name, set_name] = useState('');
+    const [debtor, set_debtor] = useState('');
+    const [amount, set_amount] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        submit({
+            'title': name,
+            'from': debtor,
+            'amount': amount
+        })
+    };
+
+    return (
+        <div>
+            <form>
+                <label>
+                    Debt name:
+                    <input type="text" value={name}
+                        onChange={(e) => set_name(e.target.value)} />
+                </label>
+                <label>
+                    Debtor:
+                    <input type="text" value={debtor}
+                        onChange={(e) => set_debtor(e.target.value)} />
+                </label>
+                <label>
+                    Amount:
+                    <input type="number" value={amount}
+                        onChange={(e) => set_amount(e.target.value)} />
+                </label>
+                <button onClick={handleSubmit}>Create</button>
+            </form>
+        </div>
+    )
+}
+
+function submit(data) {
+    console.log(data);
 }
 
 export default App;
---
+++ src/services.js
@@ -0,0 +1,19 @@
+export { getDebts };
+
+function getDebts() {
+    return [{
+            'id': 1,
+            'title': 'Monitor',
+            'from': 'Camilo',
+            'created': '11/30/22',
+            'amount': 200,
+            'completed': true
+        }, {
+            'id': 2,
+            'title': 'Monitor Extra',
+            'from': 'Camilardo',
+            'created': '11/30/22',
+            'amount': 150,
+            'completed': false
+        }]
+}
